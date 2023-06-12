fa2f9bef64161253782e2f3a18f2cad8fd7056e4
Crowdbotics <engineering@crowdbotics.com>
2023-06-12 06:16:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/proud_hat_dev_90647/MainActivity.java
@@ -0,0 +1,15 @@
+package com.proud_hat_dev_90647;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "proud_hat_dev_90647";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-hat-dev-90647",
+  url: "https://proud-hat-dev-90647.botics.co",
+  api: "https://proud-hat-dev-90647.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
